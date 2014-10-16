bb5d18d4600ee751334576a4e3eb37a84a74218e
Abdennebi Mohamed <abdennebi@gmail.com>
2014-10-17 00:18:38 +0200

    Add 'appengine-blobstore' project

---
+++ AppEngineLab/appengine-blobstore/src/main/java/com/github/appenginelab/Serve.java
@@ -0,0 +1,21 @@
+package com.github.appenginelab;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Serve extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+        BlobKey blobKey = new BlobKey(req.getParameter("blob-key"));
+        blobstoreService.serve(blobKey, res);
+    }
+}
